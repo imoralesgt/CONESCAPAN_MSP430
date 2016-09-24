617fbd243120052e1e73e991b305f3cf6a61b38e
Iván Morales <ivan.rene.morales@gmail.com>
2016-09-24 12:29:18 -0600

    I/O Exercise Solved
    Added support for MSP430F5529 Launchpad Development Board

--- 02 IOs/mainSolved.c
+++ 02 IOs/mainSolved.c
@@ -14,42 +14,51 @@
 //	- Variables: variableUno
 //  - Constantes: CONSTANTE_UNO
 
-// LEDs en P1.0 y P1.6
-// 0b01000001 = 0x41
+// LEDs en P1.0 y P4.7
+// 0b00000001 = 0x01
+// 0b10000000 = 0xF0
 
-// Push-Button en P1.3
-// 0b00001000 = 0x08
+// Push-Button en P2.1
+// 0b00000010 = 0x02
 
-#include <msp430g2553.h>
+#include <msp430f5529.h>
 
-const unsigned int MAX_DELAY = 20000;
+const unsigned int MAX_DELAY = 50000;
 unsigned int ledState = 0; //0 = LED Rojo, 1 = LED Verde
 unsigned int pushState = 1; //0 = Presionado, 1 = Sin Presionar
 
 int main(void) {
 	WDTCTL = WDTPW | WDTHOLD;		// Detener el Watchdog Timer
-	P1DIR |= 0x41;					// Establecer pine P1.0 y P1.6 como salida
-	P1OUT &= ~0x41;					// Inicializar pines P1.0 y P1.6 en estado bajo
 
-	P1DIR &= ~0x08;					// Definir como entrada
-	P1REN |= 0x08;					// Habilitar resistencia Pull-Up/Down
-	P1OUT |= 0x08;					// Seleccionar resistencia Pull-Up
+	//------- Inicializar el pin correspondiente al LED rojo (P1.0)
+	P1DIR |= 0x01;					// Establecer pin P1.0 como salida
+	P1OUT &= ~0x01;					// Inicializar pin P1.0 para estado en bajo
+
+	//------ Inicializar el pin correspondiente al LED verde (P4.7)
+	P4DIR |= 0xF0;
+	P4OUT &= ~0x01;
+
+	//------- Inicializar el pin correspondiente al Push-Button S1 (P2.1)
+	P2DIR &= ~0x02;					// Definir como entrada
+	P2REN |= 0x02;					// Habilitar resistencia Pull-Up/Down
+	P2OUT |= 0x02;					// Seleccionar resistencia Pull-Up
 
 	while(1){
 		volatile unsigned int i;	// variable volatil (permite optimizar uso de RAM)
 
-		pushState = (P1IN & 0x08) >> 3; //Enmascaramiento y corrimiento
+		pushState = (P2IN & 0x02) >> 1; //Enmascaramiento y corrimiento
 
-		if(!pushState){
-			//ledState = (!ledState & 0x01);
-			ledState ^= 0x01; //Cualquiera de estas opciones es correcta
+
+		if(!pushState){ //if(pushState == 0)
+			ledState = !ledState;
 		}
+
 
 		if(!ledState){ //if(state == 0)
 			P1OUT ^= 0x01; //Toggle LED Rojo
-			P1OUT &= ~0x40; //Mantener LED Verde apagado
+			P4OUT &= ~0xF0; //Mantener LED Verde apagado
 		}else{
-			P1OUT ^= 0x40; //Toggle LED Verde
+			P4OUT ^= 0xF0; //Toggle LED Verde
 			P1OUT &= ~0x01; //Mantener LED Rojo apagado
 		}
 
@@ -62,11 +71,9 @@ int main(void) {
 }
 
 /*
- * EJERCICO:
+ * EJERCICO: --- ESTE CODIGO ES LA SOLUCION AL EJERCICIO ---
  * 	Modificar el código para cumplir el siguiente objetivo:
- * 		- En vez de requerir que se mantenga presionado el Push-button,
+ * 		  En vez de requerir que se mantenga presionado el Push-button,
  * 		  el usuario solamente tiene que presionar una vez para cambiar
  * 		  el LED que parpadea.
  */
-
-// ESTE ARCHIVO YA CONTIENE EL CÓDIGO CON LA SOLUCIÓN
